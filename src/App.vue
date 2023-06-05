5db60d76c566dffa4e1e3831f10dd19e51988d82
Rohit-ML-2023 <133108862+Rohit-ML-2023@users.noreply.github.com>
2023-06-05 10:49:24 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    public/favicon.ico
    	modified:   src/App.vue
    	modified:   src/views/HomeView.Vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <template>
   <nav>
-    <!-- <router-link to="/">Home</router-link> | -->
+    <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/Prediction"> Prediction </router-link>
   </nav>
